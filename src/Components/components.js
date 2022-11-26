b3f0bfdfd7fb76af9fe811cd0da5daa89433ea7d
Diego Candido <diegocan.sou@gmail.com>
2022-11-26 11:28:12 -0300

    feat: MyProfilePage

--- src/Components/components.js
+++ src/Components/components.js
@@ -1,27 +1,57 @@
 import { Header, StyledFooter } from "../Components/styledComponents.js";
-import pokemon from "../assets/img/pokemon.png"
-import rocket from "../assets/img/rocket.png"
+import pokemon from "../assets/img/pokemon.png";
+import rocket from "../assets/img/rocket.png";
+import { AuthContext } from "../Providers/Auth.js";
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import user from "../assets/img/user.png";
 
 export function HeaderComponent() {
-    return (
-      <Header>
-            <p>Bem vindo ao PokeMarket</p>
-            <img alt = "Pikachu" src={pokemon} />
-      </Header>
-    )
+  const { userData, setUserData } = React.useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function logout() {
+    setUserData({});
+    navigate("/");
+  }
+
+  return (
+    <Header>
+      <p>Bem vindo ao PokeMarket</p>
+      <Link to="/home">
+        <img alt="Pikachu" src={pokemon} />
+      </Link>
+      <div className="user">
+        <div className="userName-Avatar">
+          <img
+            className="avatar-user"
+            alt="Icone do avatar do usuario"
+            src={userData.url === undefined ? user : userData.url}
+          />
+          <p>{userData.name}</p>
+        </div>
+        <div className="myProfile-logout">
+          <Link to="/perfil">
+            <button>Meu Perfil</button>
+          </Link>
+          <button onClick={() => logout()}>Sair</button>
+        </div>
+      </div>
+    </Header>
+  );
 }
 
-export function Footer () {
-    return (
+export function Footer() {
+  return (
     <StyledFooter>
-        <img alt = "equipe rocket" src={rocket} />
-        <p><strong>Equipe rocket decolando de novo!</strong><br></br>
-            Agora lançando nosso e-commerce <strong>PokeMarket</strong>:<br></br>
-            Um lugar onde você pode obter seu tão sonhado pokemon sem precisar se aventurar mundo a fora!
-        </p>
-        
+      <img alt="equipe rocket" src={rocket} />
+      <p>
+        <strong>Equipe rocket decolando de novo!</strong>
+        <br></br>
+        Agora lançando nosso e-commerce <strong>PokeMarket</strong>:<br></br>
+        Um lugar onde você pode obter seu tão sonhado pokemon sem precisar se
+        aventurar mundo a fora!
+      </p>
     </StyledFooter>
-    )
-    
-    
+  );
 }
--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import { Container } from "../Components/styledComponents.js";
+import { Container, LoginBox } from "../Components/styledComponents.js";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -100,61 +100,3 @@ export default function SignUpPage() {
   );
 }
 
-const LoginBox = styled.div`
-  background-color: red;
-  width: 480px;
-  height: 530px;
-  border-radius: 8px;
-  margin: 0 auto;
-  margin-top: 100px;
-  font-size: 26px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  p {
-    text-align: center;
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-    margin-top: 30px;
-
-    .inputs {
-      width: 360px;
-      height: 48px;
-      border-radius: 5px;
-      margin-top: 8px;
-      border: 1px solid #c6c6c6;
-      font-size: 22px;
-      font-family: "Fira Mono", monospace;
-    }
-
-    button {
-      margin-top: 15px;
-      height: 52px;
-      border: 1px solid #c6c6c6;
-      border-radius: 5px;
-      font-size: 22px;
-      font-family: "Fira Mono", monospace;
-    }
-  }
-
-  .button_sign-up {
-    margin-top: 30px;
-    border: none;
-    font-size: 16px;
-    background-color: red;
-    font-family: "Fira Mono", monospace;
-
-    :hover {
-      text-decoration: underline;
-    }
-  }
-
-  button:hover {
-    cursor: pointer;
-  }
-`;
